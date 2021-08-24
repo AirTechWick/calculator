39ff2f2936d9ec083f3b40ecbbf2d82d7dbb613e
AirWick <erikbolt11@gmail.com>
2021-08-23 18:05:55 -0700

    added layout for calculator

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function start()
+{
+
+}
+
+function add(a,b)
+{
+    return a + b;
+}
+
+function subtract(a,b)
+{
+    return a - b;
+}
+
+function multiply(a,b)
+{
+    return a * b;    
+}
+
+function divide(a,b)
+{
+    if(b == 0)
+    {
+        window.alert("Nope, You can't do that!");
+        return;
+    }
+
+    return a / b;
+}
